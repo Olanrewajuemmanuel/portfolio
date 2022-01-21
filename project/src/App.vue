0476e63e490240b39185e12b677480eec7066c64
Larry <larryolaleru@outlook.com>
2022-01-21 22:59:21 +0100

    - Clean-up - Landmark 1 - push to github-1

---
+++ project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="display-6 bg-dark text-primary">Hello</div>
+    <div class="row">
+      <div class="col-md-8 bg-secondary">One</div>
+      <div class="col-md-4 bg-info">Two</div>
+    </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+
---
+++ project/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Bootstrap CSS and support
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js';
+import 'popper.js/dist/popper.min.js';
+
+createApp(App).mount('#app')
